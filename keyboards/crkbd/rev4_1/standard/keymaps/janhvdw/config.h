630da46e50ba4852e2171fa0a75ed2d11bf96e2a
Jan van_der_Westhuizen <jan.vander.westhuizen@capgemini.com>
2025-08-12 08:33:39 +0200

    fix FLOW_TAP_TERM redefined error

--- keyboards/crkbd/rev4_1/standard/keymaps/janhvdw/config.h
+++ keyboards/crkbd/rev4_1/standard/keymaps/janhvdw/config.h
@@ -30,6 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 #define QUICK_TAP_TERM 80
+#undef FLOW_TAP_TERM
 #define FLOW_TAP_TERM 150
 
 #define PERMISSIVE_HOLD
