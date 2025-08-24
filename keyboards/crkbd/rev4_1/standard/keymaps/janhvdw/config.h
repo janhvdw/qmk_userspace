7115a2c3a994cc77a87da6fe9258bbb056d8b590
Jan van_der_Westhuizen <jan.vander.westhuizen@capgemini.com>
2025-08-24 10:17:38 +0200

    remove lighting support

--- keyboards/crkbd/rev4_1/standard/keymaps/janhvdw/config.h
+++ keyboards/crkbd/rev4_1/standard/keymaps/janhvdw/config.h
@@ -37,16 +37,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 
 #define USE_SERIAL_PD2
-#ifdef RGBLIGHT_ENABLE
-#    undef RGBLIGHT_LED_COUNT
-#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_LED_COUNT 54
-#    undef RGBLED_SPLIT
-#    define RGBLED_SPLIT {27, 27}
-#    define RGBLIGHT_LIMIT_VAL 120
-#    define RGBLIGHT_HUE_STEP 10
-#    define RGBLIGHT_SAT_STEP 17
-#    define RGBLIGHT_VAL_STEP 17
-#endif
-
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
